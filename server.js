7111f2069ba2f541fe91c4069cbe3c681c187763
ady hu <fisker112@gmail.com>
2017-06-18 02:03:23 +0000

    1

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-var app = require('express');
+var express = require('express');
+var app = express();
 
+app.use('/client', express.static(process.cwd() + '/client'));
+app.route('/me').get((req,res,next)=>{
+  var agent = req.get('User-Agent');
+  res.send(agent);
+  next();
+})
 
+app.listen(process.env.PORT, function () {
+  console.log('Node.js listening ...');
+});
